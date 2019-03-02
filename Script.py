4179ca973b22988f871ced00c17a4a7040f23bb0
Ibrar Munir <ibrarmunir009@gmail.com>
2019-03-02 15:14:26 +0500

    Added test objects

--- Script.py
+++ Script.py
@@ -1,1 +1,2 @@
-print("Hello")
+destinations = ["Paris,France", "Shanghai,China", "Los Angeles,USA", "Sao Paulo,Brazil"]
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
